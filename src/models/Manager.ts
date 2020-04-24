995a57791eec523ea792f8320d8bc214d5612b09
Max <max.09@outlook.com>
2020-04-24 15:29:10 +0200

    calculate managers salaries

--- src/models/Manager.ts
+++ src/models/Manager.ts
@@ -1,10 +1,22 @@
 import {Employee} from "./Employee";
 
 export class Manager extends Employee {
+  subordinates: Array<Employee> = [];
 
   constructor(name: string) {
     super(name, null)
   }
 
-  subordinates: Array<Employee>;
+  addSubordinate(employee: Employee){
+    this.subordinates.push(employee);
+    this.calculateAndSetSalary();
+  };
+
+  calculateAndSetSalary(){
+    let sumSubordinatesSalary: number = 0;
+    for (let sub of this.subordinates){
+      sumSubordinatesSalary += sub.salary;
+    }
+    this.salary = sumSubordinatesSalary + 300;
+  }
 }
